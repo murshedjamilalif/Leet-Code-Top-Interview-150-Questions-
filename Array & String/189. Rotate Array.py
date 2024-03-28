86df31fa1d8d8d8ff02a59ff738169af086ee484
Murshed Jamil Alif <murshedjamilalif@gmail.com>
2024-03-29 04:36:08 +0600

    Create 189. Rotate Array.py

---
+++ Array & String/189. Rotate Array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        k = k % len(nums)
+        n = len(nums)
+        i = 0
+        count = 0
+        while count < n:
+            pos = (i + k) % len(nums)
+            curr = nums[pos]
+            nums[pos] = nums[i]
+            count += 1
+            j = pos
+            while j != i and count < n:
+                pos = (j + k) % len(nums)
+                nums[pos], curr = curr, nums[pos]
+                j = pos
+                count += 1
+            i += 1
