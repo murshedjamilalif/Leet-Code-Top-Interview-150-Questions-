1620e3788b200db9a8b07f90ab8341041777bc55
Murshed Jamil Alif <murshedjamilalif@gmail.com>
2024-03-29 04:46:15 +0600

    Update 189. Rotate Array.py

--- Array & String/189. Rotate Array.py
+++ Array & String/189. Rotate Array.py
@@ -16,3 +16,15 @@ class Solution(object):
                 j = pos
                 count += 1
             i += 1
+
+# Test case 1: Basic rotation
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+Solution().rotate(nums, k)
+print(nums) # Expected output: [5, 6, 7, 1, 2, 3, 4]
+
+# Test case 2: Rotation with k equal to the length of the array
+nums = [-1,-100,3,99]
+k = 2
+Solution().rotate(nums, k)
+print(nums) # Expected output: [4, 5, 6, 7, 1, 2, 3]
